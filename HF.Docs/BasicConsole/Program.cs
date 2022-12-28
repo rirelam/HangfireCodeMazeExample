dfa6d530da4954f338276c2356dd06258fa9d156
Ricardo Lamar Meneses <r.lamar@habanos.cu>
2022-12-28 16:37:49 -0500

    de la documentacion

---
+++ HF.Docs/BasicConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Hangfire;
+using Hangfire.SqlServer;
+
+Console.WriteLine("Hello, World!");
+
+GlobalConfiguration.Configuration
+.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+.UseColouredConsoleLogProvider()
+.UseSimpleAssemblyNameTypeSerializer()
+.UseRecommendedSerializerSettings()
+.UseSqlServerStorage("Server=localhost\\sql2019e;Database=Hangfire.Sample; Integrated Security = True; ",
+  new SqlServerStorageOptions
+    {
+        CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+        SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+        QueuePollInterval = TimeSpan.Zero,
+        UseRecommendedIsolationLevel = true
+    });
+
+BackgroundJob.Enqueue(() => Console.WriteLine("Hello, Job!"));
+BackgroundJob.Schedule(() => Console.WriteLine("Hello from Sechedule"), TimeSpan.FromSeconds(10));
+
+using (var server = new BackgroundJobServer())
+{
+    Console.ReadLine();
+}
---
+++ HF.Docs/BasicWebapi/Program.cs
@@ -0,0 +1,55 @@
+using Hangfire;
+using Hangfire.SqlServer;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Hangfire services.
+builder.Services.AddHangfire(configuration => configuration
+    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection"), new SqlServerStorageOptions
+    {
+        CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+        SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+        QueuePollInterval = TimeSpan.Zero,
+        UseRecommendedIsolationLevel = true,
+        DisableGlobalLocks = true
+    })
+    );
+// Add the processing server as IHostedService
+builder.Services.AddHangfireServer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseHangfireDashboard();
+app.MapHangfireDashboard();
+
+IBackgroundJobClient backgroundJobs = new BackgroundJobClient();
+IRecurringJobManager recurringJobManager = new RecurringJobManager();
+
+backgroundJobs.Enqueue(() => Console.WriteLine("Hello world from Hangfire!"));
+recurringJobManager.AddOrUpdate("recurringId", () => Console.WriteLine("Hey I'm running every minute."), Cron.Minutely);
+
+app.Run();
